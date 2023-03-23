7bab89d6dde31930da72878fb2ed312996456f06
Frank <f.kinuthia01@gmail.com>
2023-03-23 09:44:52 +0300

    Done & dusted

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+    case array.length
+    when 0
+        ""
+        # The method first checks the length of the array and returns an empty string if the array is empty. 
+    when 1
+        array[0]
+        # If there's only one element in the array, it returns that element. 
+    when 2
+        "#{array[0]} and #{array[1]}"
+        # If there are two elements in the array, it combines them with the word "and". 
+    else
+        # If there are more than two elements in the array, it removes the last element from the array and stores it in a variable called last_word. 
+        # It then joins the remaining elements in the array with commas and spaces using join(", "), and adds ", and" before the last word. The resulting string is returned. 
+        last_word = array.pop
+        oxford_string = array.join(", ")
+        oxford_string +=  ", and #{last_word}"
+    end 
 end
+
+# This creates a properly formatted string using the Oxford Comma.
